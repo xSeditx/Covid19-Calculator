ed76972d5e94f71b2ca9eb7819439b64e4a6399e
Sedit <curtis.turner@live.com>
2020-03-14 20:23:38 -0400

    Got parser mostly working. Bit of an error escaping on quoted , characters. Added test file Cases.csv

--- Covid19-Calculator/Covid19calc.h
+++ Covid19-Calculator/Covid19calc.h
@@ -1,17 +1,207 @@
 #pragma once
+#include<string>
 
 #include"csv.h"
 
+/* City State where Outbreak is taking place */
+struct Location_t
+{
+    Location_t() = default;
+    Location_t
+    (
+        std::string _province,
+        std::string _region,
+        float       _latitude,
+        float       _longitude
+    )
+        :
+        Province(_province),
+        Region(_region),
+        Latitude(_latitude),
+        Longitude(_longitude)
+    {}
+
+    std::string Province;
+    std::string Region;
+    float Latitude;
+    float Longitude;
+};
+
+/* Year / Day / Month : Time Data */
+struct Date_t
+{
+    Date_t() = default;
+    Date_t
+    (
+        uint32_t _year,
+        uint32_t _month,
+        uint32_t _day,
+        uint32_t _hours,
+        uint32_t _minute,
+        uint32_t _seconds
+    )
+        :
+        Year(_year),
+        Month(_month),
+        Day(_day),
+        Time({ _hours, _minute, _seconds })
+    {}
+
+    uint32_t Year  { 2020 };
+    uint32_t Month ;
+    uint32_t Day;
+
+    /* Hours / Minutes / Seconds / data */
+    struct Time_t
+    {
+        Time_t() = default;
+        Time_t
+        (
+            uint32_t _hours,
+            uint32_t _minute,
+            uint32_t _seconds
+        )
+            :
+            Hours(_hours),
+            Minute(_minute),
+            Seconds(_seconds)
+        {}
+
+        uint32_t Hours  { 0 };
+        uint32_t Minute { 0 };
+        uint32_t Seconds{ 0 };
+    }Time;
+
+    static Date_t parse_DateTimeString(std::string _date)
+    {
+        Date_t result;
+
+        size_t readPos{ 0 };
+        std::string delimiter = "-";
+
+        size_t Count = _date.find(delimiter);
+        std::string yearStr = _date.substr(0, Count);  
+        result.Year = std::stoi(yearStr);
+
+        std::string monthStr = _date.substr(5, 2);  
+        result.Month = std::stoi(monthStr);
+
+        std::string dayStr = _date.substr(8, 2);  
+        result.Day = std::stoi(dayStr);
+
+        size_t TimeStart = _date.find('T') + 1;
+        std::string hourStr = _date.substr(TimeStart, 2);
+        result.Time.Hours = std::stoi(hourStr);
+
+        std::string minStr = _date.substr(TimeStart+3, 2);
+        result.Time.Minute = std::stoi(minStr);
+
+        std::string secStr = _date.substr(TimeStart+6, 2);
+        result.Time.Seconds = std::stoi(secStr);
+
+        return result;
+    }
+};
+std::string s = "scott>=tiger";
 
 
 
 
 
 
+/* Location / Confirmed Cases / Deaths / Recovered :  Date data*/
+struct Outbreak_info
+{
+    Outbreak_info() = default;
+    Outbreak_info
+    (
+        Location_t _place,
+        uint32_t   _confirmed,
+        uint32_t   _deaths,
+        uint32_t   _recovered,
+        Date_t     _date_Time
+    )
+        :
+        Place(_place),
+        Confirmed(_confirmed),
+        Deaths(_deaths),
+        Recovered(_recovered),
+        Date_Time(_date_Time)
+    {}
+
+    Location_t Place;
+    uint32_t   Confirmed;
+    uint32_t   Deaths;
+    uint32_t   Recovered;
+    Date_t     Date_Time;
+};
 
 
 
 
+class Pandemic_Map
+{
+public:
+    std::vector<Outbreak_info> Outbreak_List;
+     
+
+    void load_OutbreakData(std::string _filename)
+    {
+
+        // template<
+        //     unsigned column_count,
+        //     class trim_policy = trim_chars<' ', '\t'>,
+        //     class quote_policy = no_quote_escape<','>,
+        //     class overflow_policy = throw_on_overflow,
+        //     class comment_policy = no_comment
+        // >
+            
+        io::CSVReader<8, io::trim_chars<' ', '\t'>, io::no_quote_escape<','>> in("Cases.csv");
+        in.read_header
+        (
+           io::ignore_extra_column,/// io::ignore_no_column , ///
+            "Province/State",
+            "Country/Region",
+            "Last Update",
+            "Confirmed",
+            "Deaths",
+            "Recovered",
+            "Latitude",
+            "Longitude"
+        );
+
+        Outbreak_info Row_data;
+
+        std::string DateTimeIN;
+        float Long{ 0 }, Lat{ 0 };
+         while (
+             in.read_row(
+             Row_data.Place.Province,
+             Row_data.Place.Region,
+             DateTimeIN, 
+             Row_data.Confirmed,
+             Row_data.Deaths,
+             Row_data.Recovered,
+             Long,
+             Lat))
+         {
+             Row_data.Date_Time = Date_t::parse_DateTimeString(DateTimeIN);
+             Outbreak_List.push_back(Row_data);
+             // do stuff with the data
+         }
+    }
+};
+
+//  "Province / State",
+//  "Country / Region",
+//  "Last Update"     ,
+//  "Confirmed"	      ,
+//  "Deaths"          ,
+//  "Recovered"	      ,
+//  "Latitude"        ,
+//  "Longitude"       ,
+
+//Province / State	Country / Region	Last Update	Confirmed	Deaths	Recovered	Latitude	Longitude
 
 
 
--- Covid19-Calculator/main.cpp
+++ Covid19-Calculator/main.cpp
@@ -6,6 +6,10 @@
 
 int main()
 {
-
+   
+    //TODO: Parser is escaping on "," Chars despite being explicitly told not to. 
+    Pandemic_Map Map;
+    Map.load_OutbreakData("Cases.csv");
+    {}
     return 0;
 }
